5aacc8d04c3f54eba6b5c797cf19b027de91e8d3
bcd <bcvanduuren@gmail.com>
2013-12-29 16:29:52 +0100

    solved first binary tree problem

---
+++ binarytrees.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* NewNode(int data) {
+    struct node* node = malloc(sizeof(struct node));
+
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+int lookup(struct node* node, int target) {
+    if (!node) return 0;
+    else {
+        if (target == node->data) return 1;
+        else {
+            if (target < node->data) return lookup(node->left, target);
+            else return lookup(node->right, target);
+        }
+    }
+}
+
+struct node* insert(struct node* node, int data) {
+    if (!node) return NewNode(data);
+    else {
+        if (data <= node->data) node->left = insert(node->left, data);
+        else node->right = insert(node->right, data);
+    }
+
+    return node;
+}
+
+/********************
+ ** btree problems **
+ ********************/
+
+struct node* build123_a() {
+    struct node* node1 = NewNode(1);
+    struct node* node2 = NewNode(2);
+    struct node* node3 = NewNode(3);
+
+    node2->left = node1;
+    node2->right = node3;
+
+    return node2;
+}
+
+struct node* build123_b() {
+    struct node* root = NewNode(2);
+
+    root->left = NewNode(1);
+    root->right = NewNode(3);
+
+    return root;
+}
+
+struct node* build123_c() {
+    struct node* root = insert(NULL, 2);
+    insert(root, 1);
+    insert(root, 3);
+
+    return root;
+}
+
+int main() {
+    struct node* root = build123_c();
+
+    printf("root->data: %d\nroot->left->data: %d\nroot->right->data: %d\n",
+        root->data, root->left->data, root->right->data);
+//    if (lookup(root, 3)) printf("found!\n");
+
+    return 0;
+}
